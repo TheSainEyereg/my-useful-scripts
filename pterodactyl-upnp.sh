0165ba92650669a1db7a40121e2208b13910607f
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2025-09-23 12:44:01 +0300

    Update pterodactyl-upnp.sh

--- pterodactyl-upnp.sh
+++ pterodactyl-upnp.sh
@@ -4,7 +4,7 @@ PANEL_URL="https://panel.example.com"
 API_KEY="YOUR_TOKEN" # Application API key with read access to allocations
 NODE_ID=1
 
-IGNORE_ALIAS=("dummy") # array of ignored aliases
+IGNORE_ALIAS=("ignore" "local") # array of ignored aliases
 IGNORE_IP=("127.0.0.1") # array of ignored allocations
 
 UPNP_DURATION=600 # 10 minutes
@@ -21,7 +21,7 @@ res=$(curl -s "$PANEL_URL/api/application/nodes/$NODE_ID/allocations" \
 function processRow() {
 	ip=$1
 	alias=$2
-	assigned=$3	
+	assigned=$3
 	port=$4
 }
 
@@ -38,10 +38,16 @@ jq -r ".data[].attributes | {ip, alias, assigned, port} | map(.) | @sh" <<< "$re
 
 	if [[ $assigned == "true" ]]; then
 		label="$ip:$port ($alias)"
-
 		echo "$label is assigned"
 
-		for proto in "TCP" "UDP"; do
+		first_word=$(awk '{print $1}' <<< "$alias")
+		if [[ "$first_word" == "TCP" || "$first_word" == "UDP" ]]; then
+			proto_array=("$first_word")
+		else
+			proto_array=("TCP" "UDP")
+		fi
+
+		for proto in "${proto_array[@]}"; do
 			upnpc -e "[ptero-upnp] $proto $label" -a $LOCAL_IP $port $port $proto $UPNP_DURATION | tail -n 1
 		done
 	fi
